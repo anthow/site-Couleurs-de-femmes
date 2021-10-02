a8141cbc450c32430da1adf28ff9013dee2b330f
anthow <anthonyenglebert@gmail.com>
2021-10-02 09:00:21 +0200

    plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,29 @@ module.exports = {
       },
     },
 
+
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+
+          "/*": [
+            "Basic-Auth: someuser:somepassword anotheruser:anotherpassword",
+          ],
+          "/my-page": [
+            // matching headers (by type) are replaced by Netlify with more specific routes
+            "Basic-Auth: differentuser:differentpassword",
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+
     {
       resolve: `gatsby-source-datocms`,
       options: {
